87ed673979ef816d5611f61c0b958080976f9c74
reric <igor.roy@gmail.com>
2020-02-02 23:25:09 +0200

    added new topics

--- src/main/java/com/demo/SpringBootWithKafkaApplication.java
+++ src/main/java/com/demo/SpringBootWithKafkaApplication.java
@@ -2,9 +2,27 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
 
 @SpringBootApplication
 public class SpringBootWithKafkaApplication {
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory kafkaListenerContainerFactory(
+			ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+			ConsumerFactory<Object, Object> kafkaConsumerFactory,
+			KafkaTemplate<Object, Object> template) {
+		ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		configurer.configure(factory, kafkaConsumerFactory);
+		factory.setErrorHandler(new SeekToCurrentErrorHandler(
+				new DeadLetterPublishingRecoverer(template), 3));
+		return factory;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithKafkaApplication.class, args);
--- src/main/java/com/demo/engine/Consumer.java
+++ src/main/java/com/demo/engine/Consumer.java
@@ -5,15 +5,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Service
 public class Consumer {
 
     private final Logger logger = LoggerFactory.getLogger(Producer.class);
 
     @KafkaListener(topics = "users", groupId = "group_id")
-    public void consume(String message) throws IOException {
+    public void consume(String message) {
         logger.info(String.format("#### -> Consumed message -> %s", message));
     }
+
+    @KafkaListener(id = "fooGroup", topics = "topic1")
+    public void listen(String in) {
+        logger.info("Received: " + in);
+        if (in.startsWith("foo")) {
+            throw new RuntimeException("failed");
+        }
+    }
+
+    @KafkaListener(id = "dltGroup", topics = "topic1.DLT")
+    public void dltListen(String in) {
+        logger.info("Received from DLT: " + in);
+    }
+
 }
